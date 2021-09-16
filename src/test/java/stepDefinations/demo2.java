4e78c515f06e8534829f619424a6404a9cd77c12
Arvind <arvindkush1987@gmail.com>
2021-09-15 22:25:02 -0500

    3rd commit from india architect

--- src/test/java/stepDefinations/demo2.java
+++ src/test/java/stepDefinations/demo2.java
@@ -3,7 +3,7 @@ package stepDefinations;
 public class demo2 {
 
 	public static void main(String[] args) {
-		System.out.println("2 commited changed from india user x");
+		System.out.println("2  commited changed from india user x");
 	}
 
 }
---
+++ src/test/java/stepDefinations/demo6.java
@@ -0,0 +1,11 @@
+package stepDefinations;
+
+public class demo6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("3  commited changed from india user x");
+	}
+	}
+
+
